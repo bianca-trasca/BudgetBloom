9572074037ddc4ae27d047fb9d5309664de7c63e
Bianca Trasca <biancatrascaa@gmail.com>
2023-11-21 11:17:18 +0200

    Creatd simple navbar component without interactions

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import "./App.css";
-import Navbar from "./components/UI components/Navbar";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home page/Home";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Routes,
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" exact Component={Home} />
+        </Routes>
+      </Router>
     </div>
   );
 }
